af2333c7cabaf047c9fea459f26281f490d42b04
Henrik Maurin <henrik@henrikmaurin.se>
2020-12-06 10:58:02 +0100

    Day 6

--- AdventOfCode2020/Days/Day6.cs
+++ AdventOfCode2020/Days/Day6.cs
@@ -1,23 +1,121 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode2020.Days
 {
-    public class Day6
-    {
-		public static void Problem1()
+	public class Day6
+	{
+		public static int Problem1()
 		{
-			
-			Console.WriteLine("Problem1");
+			List<string> data = File.ReadAllLines("Data/Day6.txt").ToList();
+
+			int result = Process(data);
+
+			Console.WriteLine(result);
+			return result;
 		}
 
-		public static void Problem2()
+		public static int Problem2()
 		{
-			
-			Console.WriteLine("Problem2");
+			List<string> data = File.ReadAllLines("Data/Day6.txt").ToList();
+
+			int result = Process2(data);
+
+			Console.WriteLine(result);
+			return result;
+		}
+
+		public static int Process(List<string> answers)
+		{
+			int sum = 0;
+			Declaration declaration = null;
+			foreach (string line in answers)
+			{
+				if (declaration == null)
+					declaration = new Declaration();
+
+				if (string.IsNullOrWhiteSpace(line))
+				{
+					sum += declaration.Sum();
+					declaration = null;
+				}
+				else
+				{
+					declaration.Fill(line);
+				}
+			}
+			if (declaration != null)
+				sum += declaration.Sum();
+
+			return sum;
+		}
+
+		public static int Process2(List<string> answers)
+		{
+			int sum = 0;
+			Declaration declaration = null;
+			foreach (string line in answers)
+			{
+				if (declaration == null)
+					declaration = new Declaration();
+
+				if (string.IsNullOrWhiteSpace(line))
+				{
+					sum += declaration.SumAllYes();
+					declaration = null;
+				}
+				else
+				{
+					declaration.Fill(line,Declaration.Mode.Add);
+				}
+			}
+			if (declaration != null)
+				sum += declaration.SumAllYes();
+
+			return sum;
+		}
+
+		public class Declaration
+		{
+			public Declaration()
+			{
+				Answers = new int[26];
+				Passengers = 0;
+			}
+
+			public void Set(char question)
+			{
+				Answers['z' - question] = 1;
+			}
+
+			public void Add(char question)
+			{
+				Answers['z' - question] += 1;
+			}
+
+			public void Fill(string answers, Mode mode = Mode.Set)
+			{
+				Passengers++;
+				foreach (char answer in answers)
+					if (mode == Mode.Set)
+						Set(answer);
+					else
+						Add(answer);
+			}
+
+			public enum Mode
+			{
+				Set = 0,
+				Add = 1
+			}
+
+			public int Sum() { return Answers.Sum(); }
+			public int SumAllYes() { return Answers.Where(a => a == Passengers).Count(); }
+
+			private int[] Answers { get; set; }
+			private int Passengers { get; set; }
 		}
 	}
 }
---
+++ Tests/UnitTestDay6.cs
@@ -0,0 +1,79 @@
+﻿using AdventOfCode2020.Days;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Tests
+{
+	[TestClass]
+	public class UnitTestDay6
+	{
+		[TestMethod]
+		public void Example1()
+		{
+			List<string> data = new List<string>
+			{
+				"abc",
+				"",
+				"a",
+				"b",
+				"c",
+				"",
+				"ab",
+				"ac",
+				"",
+				"a",
+				"a",
+				"a",
+				"a",
+				"",
+				"b"
+			};
+
+			int result = Day6.Process(data);
+
+			Assert.AreEqual(11, result);
+		}
+
+		[TestMethod]
+		public void Example2()
+		{
+			List<string> data = new List<string>
+			{
+				"abc",
+				"",
+				"a",
+				"b",
+				"c",
+				"",
+				"ab",
+				"ac",
+				"",
+				"a",
+				"a",
+				"a",
+				"a",
+				"",
+				"b"
+			};
+
+			int result = Day6.Process2(data);
+
+			Assert.AreEqual(6, result);
+		}
+
+		[TestMethod]
+		public void Promblem1()
+		{
+			Assert.AreEqual(7120, Day6.Problem1());
+		}
+
+		[TestMethod]
+		public void Promblem2()
+		{
+			Assert.AreEqual(3570, Day6.Problem2());
+		}
+
+
+
+	}
+}
