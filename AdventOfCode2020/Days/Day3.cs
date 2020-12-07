ae327372e32ca48b0b6098c5b232e99b8ad95147
Henrik Maurin <henrik@henrikmaurin.se>
2020-12-07 22:25:04 +0100

    Day 3 Refactor and cleanup

--- AdventOfCode2020/Days/Day3.cs
+++ AdventOfCode2020/Days/Day3.cs
@@ -10,16 +10,17 @@ namespace AdventOfCode2020.Days
 		public static int Problem1()
 		{
 			List<string> mapData = File.ReadAllLines("Data/Day3.txt").ToList();
-			Map map = ReadMap(mapData);
+			Map map = Map.MapFactory(mapData);
 			int result = CountTrees(3, 1, map);
 
 			Console.WriteLine(result);
 			return result;
 		}
+
 		public static long Problem2()
 		{
 			List<string> mapData = File.ReadAllLines("Data/Day3.txt").ToList();
-			Map map = ReadMap(mapData);
+			Map map = Map.MapFactory(mapData);
 
 			long result = CountTrees(1, 1, map);
 			result *= CountTrees(3, 1, map);
@@ -30,6 +31,7 @@ namespace AdventOfCode2020.Days
 			Console.WriteLine(result);
 			return result;
 		}
+
 
 		public static int CountTrees(int x, int y, Map map)
 		{
@@ -44,43 +46,49 @@ namespace AdventOfCode2020.Days
 			{
 				xpos += x;
 				ypos += y;
-				if (GetAt(xpos, ypos, map) == '#')
+				if (map[xpos, ypos] == '#')
 					trees++;
 			}
 
 			return trees;
 		}
 
-		public static char GetAt(int x, int y, Map map)
-		{
-			return map.Data[x % map.MapSizeX, y];
-		}
-
-		public static Map ReadMap(List<string> mapLines)
-		{
-			Map map = new Map();
-
-			map.MapSizeX = mapLines[0].Count();
-			map.MapSizeY = mapLines.Count();
-
-			map.Data = new char[map.MapSizeX, map.MapSizeY];
-			int x = 0;
-			int y = 0;
-			foreach (string mapLine in mapLines)
-			{
-				foreach (char mapChar in mapLine)
-				{
-					map.Data[x, y] = mapChar;
-					x++;
-				}
-				y++;
-				x = 0;
-			}
-			return map;
-		}
-
 		public class Map
 		{
+			public Map(int sizeX, int sizeY)
+			{
+				MapSizeX = sizeX;
+				MapSizeY = sizeY;
+				Data = new char[MapSizeX, MapSizeY];
+			}
+
+			public static Map MapFactory(List<string> mapData)
+			{
+				Map map = new Map(mapData[0].Count(), mapData.Count());
+
+				map.Data = new char[map.MapSizeX, map.MapSizeY];
+				int x = 0;
+				int y = 0;
+				foreach (string mapLine in mapData)
+				{
+					foreach (char mapChar in mapLine)
+					{
+						map.Data[x, y] = mapChar;
+						x++;
+					}
+					y++;
+					x = 0;
+				}
+				return map;
+			}
+
+			private char GetAt(int x, int y)
+			{
+				return Data[x % MapSizeX, y];
+			}
+
+			public char this[int x, int y] { get => GetAt(x, y); }
+
 			public char[,] Data;
 			public int MapSizeX;
 			public int MapSizeY;
--- Tests/UnitTestDay3.cs
+++ Tests/UnitTestDay3.cs
@@ -1,14 +1,16 @@
 ﻿using AdventOfCode2020.Days;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using static AdventOfCode2020.Days.Day3;
 
 namespace Tests
 {
 	[TestClass]
 	public class UnitTestDay3
 	{
-		[TestMethod]
-		public void Example1()
+		[TestMethod("Day 3, Part 1")]
+		[TestCategory("Example data")]
+		public void Part1()
 		{
 			List<string> mapdata = new List<string>
 			{
@@ -25,11 +27,22 @@ namespace Tests
 				".#..#...#.#"
 			};
 
-			int result = Day3.CountTrees(3, 1, Day3.ReadMap(mapdata));
+			Map map = Map.MapFactory(mapdata);
+
+			int result = Day3.CountTrees(3, 1, map);
 			Assert.AreEqual(7, result);
 		}
 
-		[TestMethod]
+		[TestMethod("Day 3, Part 1")]
+		[TestCategory("Answer")]
+		public void Problem1()
+		{
+			int answer = 286;
+			Assert.AreEqual(answer, Day3.Problem1());
+		}
+
+		[TestMethod("Day 3, Part 2")]
+		[TestCategory("Example data")]
 		public void Example2()
 		{
 			List<string> mapdata = new List<string>
@@ -47,7 +60,7 @@ namespace Tests
 				".#..#...#.#"
 			};
 
-			Day3.Map map = Day3.ReadMap(mapdata);
+			Map map = Map.MapFactory(mapdata);
 
 			int result = Day3.CountTrees(1, 1, map);
 			result *= Day3.CountTrees(3, 1, map);
@@ -58,16 +71,12 @@ namespace Tests
 			Assert.AreEqual(336, result);
 		}
 
-		[TestMethod]
-		public void Problem1()
-		{
-			Assert.AreEqual(286, Day3.Problem1());
-		}
-
-		[TestMethod]
+		[TestMethod("Day 3, Part 2")]
+		[TestCategory("Answer")]
 		public void Problem2()
 		{
-			Assert.AreEqual(3638606400, Day3.Problem2());
+			long answer = 3638606400;
+			Assert.AreEqual(answer, Day3.Problem2());
 		}
 	}
 
